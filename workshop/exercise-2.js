d262930332d87170b67d26dc5d1a9d9f33620d1a
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-06-16 15:54:52 -0400

    q2

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -3,7 +3,19 @@
 // the greater one.
 
 // Define the function
-function max(num1, num2) {}
 
-// Call the function
-max(12, 43);
+const biggerr = function(num1, num2) {
+    if (num1 > num2) {
+      return num1
+    }
+    else {
+      return num2
+    }
+  }
+
+
+  // Call the function
+
+  console.log(biggerr(46,99))
+  
+
