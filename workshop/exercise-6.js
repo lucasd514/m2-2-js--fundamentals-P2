dbcb769ee8443d5f7c09b952098bb6f4901f2df0
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-06-17 09:40:21 -0400

    morning review

--- workshop/exercise-6.js
+++ workshop/exercise-6.js
@@ -7,16 +7,14 @@
 // Write a function that accepts a list of numbers and returns a new list with
 // all of the even numbers removed.
 
-// used the solution heavy for this one, as a jump off for the next ones... took me a while to 
 //sort of get it
 
-function removeEvens(lst) {
-  const evens = lst.filter(function (number) {
-    return number % 2 !== 0;
-  });
-
-  return evens;
+function nice(lst){
+  const noeven = lst.filter(function(number) {
+    return number % 2 !== 0});
+    return console.log(noeven)
 }
+nice(array)
 // -------------------------------------------------------------------------
 console.log('Q4 removeEvens()', removeEvens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 
